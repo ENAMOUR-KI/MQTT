942aa3d735850daf67aaebee44147860122bd4b7
Fabian <33206398+fabifighter007@users.noreply.github.com>
2022-05-17 18:31:27 +0200

    removed old code

--- websocket_to_mqtt_workaround.js
+++ websocket_to_mqtt_workaround.js
@@ -4,15 +4,13 @@ const dgram = require("dgram");
 
 const wss = new WebSocket.Server({ port: 8765 })
 const audioTopic = 'hermes/audioServer/default/audioFrame'
-const connectUrl = `mqtt://192.168.0.49:12183`
-
+const connectUrlMqtt = 'mqtt://192.168.0.49:12183'
 
 const PORT = 12333;
 const HOST = '192.168.0.49';
 const udpclient = dgram.createSocket('udp4');
 
-
-const client = mqtt.connect(connectUrl, {
+const client = mqtt.connect(connectUrlMqtt, {
   clean: true,
   connectTimeout: 4000,
   reconnectPeriod: 1000,
@@ -21,9 +19,7 @@ const client = mqtt.connect(connectUrl, {
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
-    //udpclient.send(message, 0, message.length, PORT, HOST
     client.publish(audioTopic, message)
 
   })
-  ws.send('Hello! Message From Server!!')
 })
